80102192236310a34b6b9a79b8f411a1b0ca3a46
Melissa Pignalosa <m.pignalosa@hotmail.com>
2023-09-10 16:08:55 +0800

    Added an event to clear emojis

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var box1 = document.querySelector(".box-1");
 var box2 = document.querySelector(".box-2");
 var box3 = document.querySelector(".box-3");
+var button = document.querySelector("button");
 
 document.addEventListener("keydown", function (e) {
     var key = e.key;
@@ -8,6 +9,14 @@ document.addEventListener("keydown", function (e) {
         box1.innerText= "🌻";
     } else if (key === "2") {
         box2.innerText= "🧀";
-    } else if (key === "3");
+    } else if (key === "3")
     box3.innerText= "🙆🏽";
 })
+
+button.addEventListener("click", function (e) {
+ box1.innerText= "1";
+ box2.innerText= "2";
+ box3.innerText= "3";
+})
+
+
